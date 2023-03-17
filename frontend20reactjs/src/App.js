f29acc881f8a400da42fcca23faea8401eedefb5
Daniel Sabino <dannisabino17@gmail.com>
2023-03-17 20:52:05 -0300

    Hooks de estado

--- frontend20reactjs/src/App.js
+++ frontend20reactjs/src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
-import Header from './Header'
+import React, { useState } from "react";
 
-function App() {
-  return (
-      <Header />
+function App(){
+  //Declare uma nova variavel de state, a qual chamaremos de "count"
+  //const [count, setCount] = useState(0);
+  const [nome, setNome] = useState("Joao");
+  const [numeros, setNumeros] = useState([1,2,3,4,5])
+  const [obj, setObj] = useState({nome: 'Daniel', idade: 24});
+
+  return(
+    <div>
+      {
+        numeros.map(function(val){
+          return(<p>{val}</p>)
+        })
+      }
+      <button onClick={() => setNome("Daniel")}>
+        Click me
+      </button>
+    </div>
   );
 }
 
