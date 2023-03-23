ea99119237c4005a2eb29444a55bd314868bdfd8
Daniel Sabino <dannisabino17@gmail.com>
2023-03-23 12:48:38 -0300

    Requisição automatizada na API

--- app_previsao_tempo/src/Search.js
+++ app_previsao_tempo/src/Search.js
@@ -10,15 +10,26 @@ export default Search;
 */
 
 function Search() {
-    function searchInput(){
+    function searchInput(e){
+        e.preventDefault();
         let currentValue = document.querySelector('input[name=searchInput').value;
-        /* Fazer requisição API */
+        
+        /* Requisição API */
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${currentValue}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`;
+        fetch(url).then(response => response.json()).then(data=>{
+            const {main, name, sys, weather} = data;
+            
+        })
     }   
 
     return(
         <div className="search">
             <h2>Digite a cidade que você quer saber a previsão.</h2>
-            <input placeholder="Digite a cidade" onKeyUp={searchInput} type="text" name="searchInput" />
+
+            <form onSubmit={(e)=>searchInput(e)}>
+                <input placeholder="Digite a cidade" type="text" name="searchInput" />
+                <input type="submit" value="Pesquisar" />
+            </form>
         </div>
     )
 }
